542efa129b240a186dd3b77137bfe373d5888991
Theo <theodorosladasmail@gmail.com>
2022-09-14 20:33:38 +0100

    Removed - styled variables in About Section

--- react-portofolio/src/components/AboutSection.js
+++ react-portofolio/src/components/AboutSection.js
@@ -1,6 +1,12 @@
 import React from "react";
 import home1 from "../img/home1.png";
 import styled from "styled-components";
+import {
+  StyledAbout,
+  StyledDescription,
+  StyledHide,
+  StyledImage,
+} from "../styles";
 
 const AboutSection = () => {
   return (
@@ -31,37 +37,5 @@ const AboutSection = () => {
     </StyledAbout>
   );
 };
-
-// Styles
-const StyledAbout = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const StyledDescription = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const StyledImage = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const StyledHide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
