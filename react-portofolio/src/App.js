652f82216de19d2c0cfad4c4ec45e0fc7434606d
Theo <theodorosladasmail@gmail.com>
2022-09-14 20:06:42 +0100

    Added - Global styles / AboutSection styles

--- react-portofolio/src/App.js
+++ react-portofolio/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+// Pages
 import AboutUs from "./pages/AboutUs";
+// Global Styles
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
