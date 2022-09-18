c495775e4de41a40d8527861f6bd6b48159e79ee
Theo <theodorosladasmail@gmail.com>
2022-09-19 00:45:08 +0100

    Created - Contact Us and Our Work Page / Added - react-router-dom

--- react-portofolio/src/App.js
+++ react-portofolio/src/App.js
@@ -2,15 +2,23 @@ import React from "react";
 // Pages
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 // Global Styles
 import GlobalStyle from "./components/GlobalStyle";
+// Router
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" exact element={<AboutUs />}></Route>
+        <Route path="/work" exact element={<OurWork />}></Route>
+        <Route path="/contact" exact element={<ContactUs />}></Route>
+      </Routes>
     </div>
   );
 }
